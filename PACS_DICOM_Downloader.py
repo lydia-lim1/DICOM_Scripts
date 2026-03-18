ddde87d4e2fe720a306ed123b5dbd4f8c2ab5107
Lydia Lim <125041615+lydia-lim1@users.noreply.github.com>
2026-03-18 17:25:51 +0000

    Update PACS_DICOM_Downloader.py

--- PACS_DICOM_Downloader.py
+++ PACS_DICOM_Downloader.py
@@ -4,21 +4,24 @@ Created on Wed Mar 18 10:10:07 2026, @author: lydia.lim
 
 ___________OVERVIEW:____________
     
->> Script that downloads scan data in .dcm format from an imaging archive (e.g. PACS) to folder on your computer (path: 'BASE_SAVE_DIR') based on a
-list (.csv) of accession numbers. End up with folder per accession number, containing series level sub folders containing the .dcm files.
+Download MRI DICOM studies from PACS using a CSV list of accession numbers.
 
->> This script is for MRI images, if want other types of images e.g. CT will need to replace 'MRImageStorage' SOP class with an appropriate alterative
-e.g. 'CTImageStorage'
+Inputs:
+- CSV file containing accession numbers
+- PACS AE title, IP and port
+- Local AE title recognised by PACS
+
+Outputs:
+- One folder per accession number under BASE_SAVE_DIR
+- Series-level subfolders containing .dcm files
+- Log file of retrieval progress and errors
 
 
-_______TIPS:_______________________
+_______NOTES:_______________________
 
-You will need to know AE Title ('REMOTE_AE'), IP address ('REMOTE_IP') and port number ('REMOTE_PORT') of the service class provider (e.g PACs)
-The local AE Title ('LOCAL_AE') will likely need to be recognised by the SCP, meaning you will likely need to ask your PACs manager to add 
-your calling AE Title and workstation for this script to function.
-
-If you use Fuji Synapse PACs and have the QueryRetrieveSCU tool downloaded and configured to PACs already,
-you can define 'LOCAL_AE' below as one listed on tool (e.g. 'CIMQRTOOL') 
+- Uses C-FIND to get StudyInstanceUID from accession number
+- Uses C-GET to retrieve DICOM files
+- Currently configured for MR Image Storage
 ___________________________________
 """
 
